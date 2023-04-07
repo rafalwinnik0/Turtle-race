5ae4c9ff9dfc01fdf948d998737d44e99b6c8e6b
rafalwinnik0 <130064427+rafalwinnik0@users.noreply.github.com>
2023-04-07 11:50:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ screen.setup(width=500, height=400)
 #user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter the color: ")
 colors = ["red", "orange", "yellow", "green", "blue"]
 players = ["Rafał", "Tomek", "Kuba", "Krzysiek", "Marcin"]
+turtles = []
 
 new_position = -100
 n = 0
@@ -15,7 +16,14 @@ for player in players:
     player.penup()
     new_position += 30
     player.goto(x=-230, y=-new_position)
+    turtles.append(player)
 
-# trzeba wyslac na gita poprawione
+is_on = True
+while is_on:
+    for turtle in turtles:
+        turtle.forward(10)
+        if turtle.xcor() == 230:
+            is_on = False
+
 
 screen.exitonclick()
